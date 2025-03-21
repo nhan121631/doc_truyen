0e6e81cc8d61b92a6ef0d408274613a63c64f2e2
Nhan12163 <phamphunhanpham8@gmail.com>
2025-03-21 17:25:17 +0700

    main

--- com.weblaptrinh/src/main/java/com/laptrinhjavaweb/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.laptrinhjavaweb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.laptrinhjavaweb.entity.RoleEntity;
-
-public interface RoleRepository extends JpaRepository<RoleEntity, Long>{
-	RoleEntity findByCode(String code);
-}
